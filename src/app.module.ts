337f2814975f17e64067a64f11f3503101016d7b
bottlesik <yh11445@gmail.com>
2024-03-11 17:20:44 +0900

    typeorm 설정 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,27 +2,15 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { Posts } from "./entities/posts";
-import setting from "./config/setting";
-import { join } from "path";
+import { ormconfig } from "./config/ormconfig";
 
-console.log(`join(__dirname, "/../entities/**/*.entity{.ts,.js}")`, join(__dirname, "/../src/entities/**/*{.ts,.js}"));
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: "mysql",
-      host: setting.DATABASE.HOST,
-      port: setting.DATABASE.PORT,
-      username: setting.DATABASE.USERNAME,
-      database: setting.DATABASE.NAME,
-      password: setting.DATABASE.PASSWORD,
-      entities: [join(__dirname, "/../src/entities/**/*{.ts,.js}")],
-      // migrations: [join(__dirname, "./../database/migrations/*{.ts,.js}")],
-      // seeds: [join(__dirname, "./../database/seeds/*{.ts,.js}")],
-      synchronize: true,
-      logging: true,
+    TypeOrmModule.forRootAsync({
+      useFactory() {
+        return ormconfig;
+      },
     }),
-    TypeOrmModule.forFeature([Posts]),
   ],
   controllers: [AppController],
   providers: [AppService],
