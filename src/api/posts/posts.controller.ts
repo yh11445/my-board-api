387c1c4884bcad587fefe985022cd9a4520d5757
bottlesik <yh11445@gmail.com>
2024-03-18 14:20:34 +0900

    add fileupload function -ing

--- src/api/posts/posts.controller.ts
+++ src/api/posts/posts.controller.ts
@@ -1,11 +1,9 @@
-import { entityToDto } from "src/common/decorators/swagger/common";
-import { Body, Controller, Delete, Get, Param, Post, Put, Res } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put, UploadedFile } from "@nestjs/common";
 import { PostsService } from "./posts.service";
 import { Transactional } from "typeorm-transactional";
 import { createPostSchema, deletePostSchema, getPostSchema, updatePostSchema } from "src/common/decorators/swagger/app/post.decorator";
 import { CreatePostDto } from "src/dto/posts/create-post.dto";
 import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
-import { GetPostDto } from "src/dto/posts/get-post.dto";
 import { UpdatePostDto } from "src/dto/posts/update-post.dto";
 import { PostResponse } from "src/dto/posts/post.response";
 
@@ -20,15 +18,20 @@ export class PostsController {
   @createPostSchema()
   async createPost(@Body() post: CreatePostDto) {
     const newPost = await this.postsService.createPost(post);
-    // const data = await entityToDto(GetPostDto, newPost);
     return PostResponse.toDto(newPost);
+  }
+
+  @Post("/upload")
+  async uploadFile(@Body() req: Express.Request) {
+    const file: Express.Multer.File = req.file;
+    console.log(file.originalname);
+    return { message: "File uploaded successfully" };
   }
 
   @Get("/:id")
   @getPostSchema()
   async getPost(@Param("id") id: number) {
     const post = await this.postsService.getPost(id);
-    // const data = await entityToDto(GetPostDto, post);
     return PostResponse.toDto(post);
   }
 
@@ -38,7 +41,6 @@ export class PostsController {
   async modifyPost(@Param("id") id: number, @Body() post: UpdatePostDto) {
     const result = await this.postsService.modifyPost(id, post);
     if (result !== null) {
-      // const data = await entityToDto(GetPostDto, result);
       return PostResponse.toDto(result);
     } else {
       return null;
